a073f36df1393e93f61342e3c3e695ccc0ca1a5b
Mauri de Souza Nunes <mauri870@gmail.com>
2016-01-29 13:24:27 -0200

    Add application main test

---
+++ tests/BasicSkeletonTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class BasicSkeletonTest extends Silex\WebTestCase
+{
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../app/app.php';
+        $app['debug'] = false;
+        return $app;
+    }
+
+    public function testCanViewInitialPage()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertTrue($client->getResponse()->isOk());
+
+        $this->assertCount(1, $crawler->filter('h1:contains("Welcome to Silex!")'));
+    }
+
+    public function testApplicationCanBoot()
+    {
+        $app = $this->createApplication();
+        $this->assertEquals(new Silex\Application(),$app);
+    }
+
+}
