d71f75f3a5a9960f7c71738831634fab73d55d51
Mauri de Souza Nunes <mauri870@gmail.com>
2016-01-29 14:25:39 -0200

    Moved routes to single file

---
+++ src/Http/routes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+
+$app->get('/', function() use($app) {
+    return $app['twig']->render('index.html.twig');
+});
