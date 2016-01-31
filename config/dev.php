55284166d51612d70a853c67eaa8fb72295bbb86
Mauri de Souza Nunes <mauri870@gmail.com>
2016-01-31 20:49:06 -0200

    Removed debug

--- config/dev.php
+++ config/dev.php
@@ -3,9 +3,6 @@
 // include the prod configuration
 require __DIR__.'/prod.php';
 
-// enable the debug mode
-$app['debug'] = true;
-
 $app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__ . '/../storage/cache/profiler',
     'profiler.mount_prefix' => '/_profiler'
