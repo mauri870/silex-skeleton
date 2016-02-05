bc2435590e353134e126ba66deee731c0b84db6e
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-05 07:58:05 -0200

    Disable display_errors

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  */
 
+ini_set('display_errors', 0);
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__ . '/../src/app.php';
