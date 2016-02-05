75a2fffe274f84ae180e0f9ec38746ea62c48a6c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-02-05 11:56:28 -0200

    Register the whoops service provider

--- config/dev.php
+++ config/dev.php
@@ -7,3 +7,5 @@ $app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__ . '/../storage/cache/profiler',
     'profiler.mount_prefix' => '/_profiler'
 ));
+
+$app->register(new Whoops\Provider\Silex\WhoopsServiceProvider());
